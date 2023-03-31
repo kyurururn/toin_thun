a37da39a91ffc1c0d76606f32ee79c65cf703d89
kyurururn <75336100+kyurururn@users.noreply.github.com>
2023-03-31 22:20:19 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-if(navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('iPad') > 0 || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0 ){
-    document.write('<link rel="stylesheet" type="text/css" href="main.css">');
-}else{
-    document.write('<link rel="stylesheet" type="text/css" href="pc.css">');
-}
+fetch('https://script.google.com/macros/s/AKfycbzdi3TnA2yz1fxmveJkyAop_NbXvpB2AvJ2GqYzINQDPhbYt6Q8p9UF_x1zDqliWA5-/exec')
+    .then(response => response.json())
+    .then(data => {
+        let url = data.url;
+        document.getElementById("button").href = url;
+
+        let load = document.getElementById("load");
+        load.style.opacity = 0;
+
+        setTimeout(() => {
+            load.style.display = "none"
+        },500);
+
+    })
+    .catch(error => console.error(error));
